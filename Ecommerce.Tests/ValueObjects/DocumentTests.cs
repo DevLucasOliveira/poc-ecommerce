400762c94c63f774095cfe111e6ee529c4bf4889
DevLucasOliveira <lucasoliveira.si@outlook.com>
2020-11-12 07:05:32 -0300

    Criando os commands

---
+++ Ecommerce.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,33 @@
+﻿using Ecommerce.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ecommerce.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document _validDocument;
+        private Document _invalidDocument;
+
+        public DocumentTests()
+        {
+            _validDocument = new Document("48971819847");
+            _invalidDocument = new Document("56723226787");
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.IsFalse(_invalidDocument.Valid);
+            Assert.AreEqual(1, _invalidDocument.Notifications.Count);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsValid()
+        {
+            Assert.IsTrue(_validDocument.Valid);
+            Assert.AreEqual(0, _validDocument.Notifications.Count);
+        }
+
+    }
+}
